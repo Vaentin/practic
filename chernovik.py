7fc28dea28414ca792af96313ea566a1254d67e9
Valentin Unkom <valentin.unkoom@gmail.com>
2022-11-24 13:44:47 +0400

    Lesson 33

--- chernovik.py
+++ chernovik.py
@@ -1,14 +1,22 @@
-# Реализуйте функцию join_numbers_from_range(), 
-# которая объединяет все числа из диапазона в 
-# строку и возвращает получившуюся строку:
 
-def join_numbers_from_range(start,finish):
-    result = ''
-    i = start
-    while i <= finish:
-      result = result + str(i)
-      i += 1
-    return result
+# Реализуйте функцию my_substr(), которая извлекает из строки подстроку указанной длины.
+# Она принимает на вход два аргумента (строку и длину) и возвращает подстроку, начиная с первого символа:
 
-print(join_numbers_from_range(5, 10))
 
+def my_substr(string, index_string):
+  
+  sub_string = ''
+  while len(sub_string) < index_string:
+    sub_string = string[:index_string]
+
+  
+    return sub_string 
+
+
+  
+  
+
+
+string = 'If I look back I am lost'
+print(my_substr(string, 1))  # => 'I'
+print(my_substr(string, 7))  # => 'If I lo'
